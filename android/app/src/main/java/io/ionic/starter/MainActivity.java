329d568fbcd53fb83d67a690f1ea625aaea735ec
Jameson Taylor <jameson@ionic.io>
2022-10-25 14:09:31 -0400

    Add android; Add fullscreen settings;

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,16 @@
+
+package io.ionic.starter;
+
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    }
+}
