8e651af72a9c0ee3d205f89bf7f5b4db1fa56bb8
Jameson Taylor <jameson@ionic.io>
2022-10-25 14:15:07 -0400

    Change colors for visibility;

--- src/views/FolderPage.vue
+++ src/views/FolderPage.vue
@@ -1,15 +1,15 @@
 <template>
   <ion-page>
     <ion-header :translucent="true">
-      <ion-toolbar>
+      <ion-toolbar color="primary">
         <ion-buttons slot="start">
-          <ion-menu-button color="primary"></ion-menu-button>
+          <ion-menu-button color="warning"></ion-menu-button>
         </ion-buttons>
         <ion-title>{{ $route.params.id }}</ion-title>
       </ion-toolbar>
     </ion-header>
     
-    <ion-content :fullscreen="true">
+    <ion-content :fullscreen="true" color="secondary">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">{{ $route.params.id }}</ion-title>
